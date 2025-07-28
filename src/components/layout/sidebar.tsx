ccda8944450a242ba0be2770639f96ea8a7fe342
M-Jawad338211 <jawad338211@gmail.com>
2025-07-28 13:38:22 +0500

    minor change in sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -52,7 +52,7 @@ export default function Sidebar({
         id="sidebar"
         className={clsx(
           "bg-white h-full w-full transition-all duration-200 overflow-hidden",
-          expanded ? "max-w-52" : "max-w-13"
+          expanded ? "max-w-52" : "max-w-12"
         )}
       >
         <div className="flex justify-end p-2">
